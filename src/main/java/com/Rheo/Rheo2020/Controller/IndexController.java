4d28a4e1e7c5d606cf069e33c8a0af0b23c601be
deng <q921598334@126.com>
2020-03-24 16:43:19 +0800

    no message

--- src/main/java/com/Rheo/Rheo2020/Controller/IndexController.java
+++ src/main/java/com/Rheo/Rheo2020/Controller/IndexController.java
@@ -32,8 +32,6 @@ public class IndexController {
         }
 
 
-
-
         return "index";
     }
 
---
+++ src/main/java/com/Rheo/Rheo2020/Controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.Rheo.Rheo2020.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+
+    @GetMapping("/login")
+    public String login(Model model){
+
+         return  "login";
+    }
+
+    @GetMapping("/l")
+    public String l(Model model){
+
+        return  "l";
+    }
+
+
+}
+
+
+
+
+
+
+
