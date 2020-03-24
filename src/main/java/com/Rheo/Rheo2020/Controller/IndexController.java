b9c93b2f0c7d0b786d8b3f8d3013d77d35475994
deng <q921598334@126.com>
2020-03-24 08:17:04 +0800

    创建

---
+++ src/main/java/com/Rheo/Rheo2020/Controller/IndexController.java
@@ -0,0 +1,40 @@
+package com.Rheo.Rheo2020.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+//这个是主界面
+@Controller
+public class IndexController {
+
+
+    int reFreshTime = 0;
+
+
+
+    @GetMapping("/")
+    public String index(Model model, HttpServletResponse response, HttpServletRequest request)
+    {
+
+        //发送邮件成功时给用户的的反馈
+        String info = (String) request.getSession().getAttribute("info");
+        if(info!=null && !info.equals("")){
+            model.addAttribute("info",info);
+            request.getSession().removeAttribute("info");
+        }
+
+
+
+
+        return "index";
+    }
+
+}
---
+++ src/test/java/com/Rheo/Rheo2020/ResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rheo.Rheo2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
